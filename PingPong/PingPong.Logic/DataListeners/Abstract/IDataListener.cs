a510912831a3fa08b89b1f69e5982cfbf7cbc5f1
TomerGabriel <tomergabriel@gmail.com>
2022-01-19 15:07:35 +0200

    Add data listener interface

---
+++ PingPong/PingPong.Logic/DataListeners/Abstract/IDataListener.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Net;
+
+namespace PingPong.Logic.DataListeners.Abstract
+{
+    public interface IDataListener
+    {
+        Action<byte[], IPEndPoint> OnDataFromEndPoint { get; set; }
+    }
+}
