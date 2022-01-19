abb9ef49000e15c1cd14f99eeacfb21e05f2ef0f
TomerGabriel <tomergabriel@gmail.com>
2022-01-19 17:32:28 +0200

    implement output on data handler and add output interface

---
+++ PingPong.Client/PingPong.Client.Presentation/Abstract/IOutput.cs
@@ -0,0 +1,7 @@
+﻿namespace PingPong.Client.Presentation.Abstract
+{
+    public interface IOutput<TOut>
+    {
+        void Output(TOut output);
+    }
+}
---
+++ PingPong.Client/PingPong.Client.Presentation/OnDataHandlers/OutputOnDataHandler.cs
@@ -0,0 +1,24 @@
+﻿using PingPong.Client.Logic.DataParsers.Abstract;
+using PingPong.Client.Logic.OnDataHandlers.Abstract;
+using PingPong.Client.Presentation.Abstract;
+
+namespace PingPong.Client.Presentation.OnDataHandlers
+{
+    public class OutputOnDataHandler<TData, TOut> : OnDataHandler<TData>
+    {
+        private readonly IOutput<TOut> _output;
+
+        private readonly IDataParser<TData, TOut> _dataParser;
+
+        public OutputOnDataHandler(IDataParser<TData, TOut> dataParser, IOutput<TOut> output)
+        {
+            _dataParser = dataParser;
+            _output = output;
+        }
+
+        public void OnDataEventHandler(TData data)
+        {
+            _output.Output(_dataParser.Parse(data));
+        }
+    }
+}
