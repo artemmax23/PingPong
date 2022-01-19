077553c63bf723731d39cf691c78d15f589c059d
TomerGabriel <tomergabriel@gmail.com>
2022-01-19 16:26:07 +0200

    implemeted string to byte array data parser

---
+++ PingPong/PingPong.Logic/DataParsers/StringToByteArrayDataParser.cs
@@ -0,0 +1,13 @@
+﻿using PingPong.Logic.DataParsers.Abstract;
+using System.Text;
+
+namespace PingPong.Logic.DataParsers
+{
+    public class StringToByteArrayDataParser : IDataParser<string, byte[]>
+    {
+        public byte[] Parse(string data)
+        {
+            return Encoding.ASCII.GetBytes(data);
+        }
+    }
+}
