3615adfd2b13cd659fd0d8fd4d687949f9a5877c
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 08:50:27 +0200

    implement object printer response handler

---
+++ PingPong.Server/PingPong.Server.Presentation/ResponseHandlers/OutputAndSendBackResponseHandler.cs
@@ -0,0 +1,35 @@
+﻿using PingPong.Server.Logic.DataParsers.Abstract;
+using PingPong.Server.Logic.ResponseHandlers.Abstract;
+using PingPong.Server.Presentation.Abstract;
+
+namespace PingPong.Server.Presentation.ResponseHandlers
+{
+    class OutputAndSendBackResponseHandler : IResponseHandler<string>
+    {
+        private readonly IOutput<string> _output;
+
+        private readonly IDataParser<string, object> _stringToObjectDataParser;
+
+        private readonly IDataParser<string, byte[]> _stringToByteArrayDataParser;
+
+        public OutputAndSendBackResponseHandler(IOutput<string> output,
+                                                IDataParser<string, object> stringToObjectDataParser, 
+                                                IDataParser<string, byte[]> stringToByteArrayDataParser)
+        {
+            _output = output;
+            _stringToObjectDataParser = stringToObjectDataParser;
+            _stringToByteArrayDataParser = stringToByteArrayDataParser;
+        }
+
+        public byte[] HandleData(string data)
+        {
+            var objectData = _stringToObjectDataParser.Parse(data);
+
+            _output.Output(objectData.ToString());
+
+            var dataBytes = _stringToByteArrayDataParser.Parse(data);
+
+            return dataBytes;
+        }
+    }
+}
