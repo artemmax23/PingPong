603b6c1b77545a252fadf3561945ab2e4ea2fe20
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 09:53:27 +0200

    add server

---
+++ PingPong/PingPong.Server/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using PingPong.Server.Logic.DataParsers;
+using PingPong.Server.Presentation.ResponseHandlers;
+using PingPong.Server.Logic.Servers;
+using PingPong.Server.Logic.Servers.Abstract;
+using System.Net.Sockets;
+using PingPong.Server.Common;
+
+namespace PingPong.Server
+{
+    public class Bootstrapper
+    {
+        public IServer BootstrapSocketSever()
+        {
+            var output = new ConsoleOutput<Person>();
+
+            var stringToByteArrayDataParser = new StringToByteArrayDataParser();
+
+            var stringToPersonDataParser = new JsonStringToGenericDataParser<Person>();
+
+            var onDataHandler = new OutputAndSendBackResponseHandler<string, Person>(output, stringToPersonDataParser, stringToByteArrayDataParser);
+
+            var socketServer = new TCPServer(onDataHandler);
+
+            return socketServer;
+        }
+
+    }
+}
---
+++ PingPong/PingPong.Server/ConsoleOutput.cs
@@ -0,0 +1,13 @@
+﻿using PingPong.Server.Presentation.Abstract;
+using System;
+
+namespace PingPong.Server
+{
+    public class ConsoleOutput<TOut> : IOutput<TOut>
+    {
+        public void Output(TOut output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
