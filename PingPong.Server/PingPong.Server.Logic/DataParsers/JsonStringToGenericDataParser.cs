299e42de4f9cc43af02a5485f69365fa926b51a7
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 08:55:39 +0200

    implement json object parser

---
+++ PingPong.Server/PingPong.Server.Logic/DataParsers/JsonStringToGenericDataParser.cs
@@ -0,0 +1,13 @@
+﻿using PingPong.Server.Logic.DataParsers.Abstract;
+using System.Text.Json;
+
+namespace PingPong.Server.Logic.DataParsers
+{
+    public class JsonStringToGenericDataParser<TParseTo> : IDataParser<string, TParseTo>
+    {
+        public TParseTo Parse(string data)
+        {
+            return JsonSerializer.Deserialize<TParseTo>(data);
+        }
+    }
+}
