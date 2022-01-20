966106b9b4f2d76febfccce6173e51b7033eeea2
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 08:56:42 +0200

    add person to server common

---
+++ PingPong.Server/PingPong.Server.Common/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace PingPong.Server.Common
+{
+    public class Person
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{ _name} is { _age } years old.";
+        }
+    }
+}
