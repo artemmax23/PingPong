27334fc5a6b20ebc7150d93ccf9990ac5691de17
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 10:01:00 +0200

    add server logic

---
+++ PingPong/PingPong.Server.Logic/ResponseHandlers/SendBackResponseHandler.cs
@@ -0,0 +1,20 @@
+﻿using PingPong.Server.Logic.DataParsers.Abstract;
+using PingPong.Server.Logic.ResponseHandlers.Abstract;
+
+namespace PingPong.Server.Logic.ResponseHandlers
+{
+    public class SendBackResponseHandler<TData> : IResponseHandler<TData>
+    {
+        private readonly IDataParser<TData, byte[]> _dataParser;
+
+        public SendBackResponseHandler(IDataParser<TData, byte[]> dataParser)
+        {
+            _dataParser = dataParser;
+        }
+
+        public byte[] HandleData(TData data)
+        {
+            return _dataParser.Parse(data);
+        }
+    }
+}
