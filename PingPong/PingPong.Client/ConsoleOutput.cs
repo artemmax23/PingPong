8d1eea739f8c37e5fdb00868ee4ee2e3e27b4da6
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 09:54:01 +0200

    add client

---
+++ PingPong/PingPong.Client/ConsoleOutput.cs
@@ -0,0 +1,13 @@
+﻿using PingPong.Client.Presentation.Abstract;
+using System;
+
+namespace PingPong.Client
+{
+    public class ConsoleOutput<TOut> : IOutput<TOut>
+    {
+        public void Output(TOut output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
